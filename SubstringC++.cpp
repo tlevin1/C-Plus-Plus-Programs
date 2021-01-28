68268c6198b87d684e3904a6049754e7db2e0158
tlevin1 <61256186+tlevin1@users.noreply.github.com>
2021-01-27 19:20:25 -0500

    C++ Practice

---
+++ SubstringC++.cpp
@@ -0,0 +1,29 @@
+
+// C++ program to print all possible substrings of a given string
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void subString(char str[], int n)
+{
+
+    for (int len = 1; len <= n; len++)
+    {
+
+        for (int i = 0; i <= n - len; i++)
+        {
+            int j = i + len - 1;
+            for (int k = i; k <= j; k++)
+                cout << str[k];
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    char str[] = "(abc){1}(de){2}";
+    subString(str, strlen(str));
+    return 0;
+} 
